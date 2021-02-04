252f118b12944723feb76bbe940a4cc4756116ae
jeangondorek <jeangondorek73@gmail.com>
2021-02-04 19:45:34 -0300

    Adicionando arquivos.
    Fome e sede funcionais. Integração com tokovoip/mumblevoip não testada.

---
+++ vrp_hud/fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'adamant'
+game {'gta5'}
+
+client_scripts {
+	"@vrp/lib/utils.lua",
+	"client.lua"
+}
+
+files {
+	"nui/app.js",
+	"nui/index.html",
+	"nui/style.css",
+	"nui/images/*"
+}
+
+ui_page {
+	"nui/index.html"
+}
